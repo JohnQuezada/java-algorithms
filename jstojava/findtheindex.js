a4972876343df55c31d5b877869317a0661510ef
JohnQuezada <johnaquezada@gmail.com>
2021-12-15 21:01:01 -0500

    completed js version of find the index algorithm

---
+++ jstojava/findtheindex.js
@@ -0,0 +1,14 @@
+https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+
+function search(arr, item) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == item) {
+            return i
+        }
+    }
+    return -1
+}
+
+// function search(arr, item) {
+//     return arr.indexOf(item)
+// }
